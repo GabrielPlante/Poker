a47eb77a451b16631efe591c647574c5dbfdfa19
GabrielPlante <gabriel.plante@live.fr>
2018-09-12 15:09:09 +0200

    Version double paire

--- Main.java
+++ Main.java
@@ -1,29 +1,28 @@
 import java.util.ArrayList;
 public class Main {
-    public Main(ArrayList<Carte> paquet)
+    public Main(ArrayList<Carte> paquet)//Le constructeur
     {
         for (int i = 0;i!=5;++i){
             int x = (int)(Math.random()*paquet.size());
-            cartes.add(paquet.get(x));
-            paquet.remove(x);
+            cartes.add(paquet.get(x));//On associe une carte random du paquet à la main
+            paquet.remove(x);//On l'enleve du paquet
         }
         for (Carte y:cartes){
-            System.out.print(y.nombre+" ");
+            System.out.print(y.nombre+" "); // Pour le debugage
         }
         System.out.print("\n");
     }
     public int valeur_main()
     {
         if (paire() != 0)
-            return 13+paire();//plus haute carte est entre 1 et 13
+            return 13+paire();//plus haute carte est entre 1 et 13, paire aussi.
         if (double_paire() != 0)
-            return 26+paire();
+            return 26+paire();//13+13=26
         else return plus_haute_carte();
-
     }
     private int double_paire()
     {
-        int a = 0, max = 0;
+        int a = 0, max = 0;// a:nombre de pair, max la valeur de la plus grande paire
         for (Carte y:cartes)
         {
             for (Carte x:cartes)
@@ -36,9 +35,9 @@ public class Main {
                 }
             }
         }
-        if (a == 2)
+        if (a == 2) //Si on a trouve 2 paire, on renvoit la valeur
             return max;
-        else return 0;
+        else return 0;//Sinon on revoit 0, qui sert de false
     }
     private int paire()
     {
@@ -46,7 +45,7 @@ public class Main {
         {
             for (Carte x:cartes)
             {
-                if (y.nombre == x.nombre && y.couleur != x.couleur)
+                if (y.nombre == x.nombre && y.couleur != x.couleur)//On regarde si c'est le meme nombre et pas la meme couleur
                     return x.nombre;
             }
         }
@@ -55,9 +54,9 @@ public class Main {
     private int plus_haute_carte()
     {
         int x = 0;
-        for (Carte y:cartes)
+        for (Carte y:cartes)//simple
             if (y.nombre > x) x = y.nombre;
         return x;
     }
-    private ArrayList<Carte> cartes = new ArrayList<>();
+    private ArrayList<Carte> cartes = new ArrayList<>();//La liste qui contient les cartes de la main du joueur
 }
