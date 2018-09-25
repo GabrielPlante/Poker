c565e0799e242b75b18400305e783a983b14616a
GabrielPlante <gabriel.plante@live.fr>
2018-09-25 20:26:50 +0200

    Finitions

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ public class Main {
     public Main(ArrayList<Carte> paquet)//Le constructeur
     {
         for (int i = 0;i!=5;++i){
-            cartes.add(paquet.get(0));//On associe une carte random du paquet à la main
+            cartes.add(paquet.get(0));//On associe la premiere du paquet à la main
             paquet.remove(0);//On l'enleve du paquet
         }
         for (Carte y:cartes){
-            System.out.print(y.nombre+y.couleur+" "); // Pour le debugage
+            System.out.print(Main.valeur_to_indiceDim(y.nombre)+y.couleur+" "); // Pour l'affichage de la main
         }
     }
     /*MANUEL : Implementer une nouvelle fonction :
@@ -169,19 +169,45 @@ public class Main {
     }
     private ArrayList<Carte> cartes = new ArrayList<>();//La liste qui contient les cartes de la main du joueur
 
-    public static String combinaisons(int x) {//Pour le debuggage uniquement, ne pas toucher
+    public static String combinaisons(int x) {//Permet de savoir avec la valeur de valeur_main à quoi on a affaire
         x = (x-1)/13;
         switch (x)
         {
-            case 0: return "Plus grande";
-            case 1: return "Paire";
-            case 2: return "Double paire";
-            case 3: return "Brelan";
-            case 4: return "Suite";
-            case 5: return "Couleur";
-            case 6: return "Full";
-            case 7: return "Carre";
-            case 8: return "Quinte flush";
+            case 0: return "carte la plus élevée : ";
+            case 1: return "paire de ";
+            case 2: return "double paire de ";
+            case 3: return "brelan de ";
+            case 4: return "suite de ";
+            case 5: return "couleur de ";
+            case 6: return "full de ";
+            case 7: return "carre de ";
+            case 8: return "quinte flush de ";
+            default: return "C'est pas bon";
+        }
+    }
+    public static String valeur_to_indiceDim(int x) //Pour transformer la valeur en la l'indice du diminutif de la carte
+    {
+        if (x < 10)
+            return Integer.toString(x+1);
+        switch (x)
+        {
+            case 10: return "V";
+            case 11: return "D";
+            case 12: return "R";
+            case 13: return "A";
+            default: return "C'est pas bon";
+        }
+    }
+    public static String valeur_to_indiceComplet(int x) //Pour transformer la valeur en la l'indice complet de la carte
+    {
+        if (x < 10)
+            return Integer.toString(x+1);
+        switch (x)
+        {
+            case 10: return "Valet";
+            case 11: return "Dame";
+            case 12: return "Roi";
+            case 13: return "As";
             default: return "C'est pas bon";
         }
     }
--- Table.java
+++ Table.java
@@ -11,30 +11,38 @@ class Scratch {
 
         while (paquet.size() < 10)
         {
-            if (paquet.size() == 5) System.out.println("Entrez les 5 cartes pour le joueur 2 :");
-            System.out.println("Carte n° "+(paquet.size()+(paquet.size()<5?1:-4)));
-            System.out.print("Nombre : ");
-            Scanner sc_nombre = new Scanner(System.in);
+            if (paquet.size() == 5) System.out.println("Entrez les 5 cartes pour le joueur 2 :");//Si on a finis d'entrer les cartes du premier joueur,
+            System.out.println("Carte n° "+(paquet.size()+(paquet.size()<5?1:-4)));//On passe au carte du deuxieme
+            System.out.print("Nombre : ");//On affiche à quelle carte on se trouve
+            Scanner sc_nombre = new Scanner(System.in);//Permet de recuperer la valeur donnee par l'utilisateur
             int nombre = sc_nombre.nextInt();
+            //TODO : faire un systeme permettant d'entrer V, D, R ou A a la place d'un int
             System.out.print("Couleur : ");
             Scanner sc_couleur = new Scanner(System.in);
             String couleur = sc_couleur.nextLine();
             if ((couleur.equals("Co") || couleur.equals("Ca") || couleur.equals("Tr") || couleur.equals("Pi")) && nombre >= 1 && nombre <= 13)
+            {
+                boolean nouvelle = true;
+                for (Carte x:paquet)//On verifie que la carte introduite ne soit pas un doublon
+                    if (x.nombre == nombre && x.couleur.equals(couleur))
+                        nouvelle = false;//Si c'est le cas, on l'indique
+                if (!nouvelle)
+                    continue;//Si ce n'est pas une nouvelle carte, on passe au tour de boucle suivant
                 paquet.add(new Carte(nombre, couleur));
+            }
         }
 
-        System.out.println("Main 1: ");
+        System.out.print("Main 1: ");
         Main joueur1 = new Main(paquet);//Initialisation du joueur 1 et 2
         System.out.println("");
-        System.out.println("Main 2: ");
+        System.out.print("Main 2: ");
         Main joueur2 = new Main(paquet);
         System.out.println("");
         int j1 = joueur1.valeur_main(), j2 = joueur2.valeur_main();
         String gagnant = "La main ";
-        if (j1 > j2) gagnant += "1 gagne avec "+Main.combinaisons(j1);
-        else if (j2 > j1) gagnant += "2 gagne avec "+Main.combinaisons(j2);
+        if (j1 > j2) gagnant += "1 gagne avec "+Main.combinaisons(j1)+Main.valeur_to_indiceComplet(((j1-1)%13)+1);
+        else if (j2 > j1) gagnant += "2 gagne avec "+Main.combinaisons(j2)+Main.valeur_to_indiceComplet(((j2-1)%13)+1);
         else gagnant = "Egalite !";
         System.out.print(gagnant);
-        //TODO : faire la presentation exacte demande dans le doc
     }
 }
